007b2c0d49bdc12e78bf9764e07a8e60b6bb1d3e
FandiWR <fandi.wahyur99@gmail.com>
2021-10-11 23:17:08 +0700

    done

---
+++ Ch. 10 - Problem Solving/Assets/Scripts/StarObs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StarObs : MonoBehaviour
+{
+    [SerializeField] private GameObject Star;
+    [SerializeField] private float timeOnScreen;
+    private float timer;
+    public UnityAction<StarObs> OnSquareDestroyed = delegate { };
+    // Start is called before the first frame update
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeOnScreen)
+        {
+            //Destroy(Star, timeOnScreen);
+            OnSquareDestroyed(this);
+            Star.SetActive(false);
+            timer = 0;
+        }
+    }
+}
